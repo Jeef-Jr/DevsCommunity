7f998c0aa0718bb2381283e85168289b45dcfea5
Jeef-Jr <jefferson.goncalvesfo@sptech.school>
2022-05-10 23:26:46 -0300

    Information updated in index page

--- frontend/js/Catalago/index.js
+++ frontend/js/Catalago/index.js
@@ -2,26 +2,33 @@ listarCard();
 
 function listarCard() {
   let cat = document.querySelector("#catalago");
-  let number = 6;
+  let number = 7;
   let imgs = [
-    "./assets/uma-noite-de-crime.jpg",
-    "./assets/uma-noite-de-crime-anarquia.jpg",
-    "./assets/uma-noite-de-crime-ano-eleicao.jpg",
-    "./assets/primeira-noite-de-crime.jpg",
-    "./assets/serie-uma-noite-de-crime.jpg",
-    "./assets/uma-noite-de-crime-a-fronteira.jpg",
+    "./assets/reactjs.jpg",
+    "./assets/nodejs.png",
+    "./assets/php7.png",
+    "./assets/java.png",
+    "./assets/pyton.jpg",
+    "./assets/flutter.png",
+    "./assets/dart.png",
   ];
-  let categoria = ["Filme", "Filme", "Filme", "Filme", "Serie", "Filme"];
-  let ano = ["2013", "2014", "2016", "2018", "2018-2019", "2021"];
+  let categoria = [
+    "React JS",
+    "Node JS",
+    "PHP 7",
+    "Java",
+    "Pyton",
+    "Flutter",
+    "Dart",
+  ];
 
   for (var i = 0; i < number; i++) {
     cat.innerHTML += `
     <div class="cards">
         <h3>${categoria[i]}</h3>
-        <div class="card-filmes">
+        <div class="card-framework">
             <img class="img-cards" src='${imgs[i]}'/>
         </div>
-        <h3>${ano[i]}</h3>
     </div>`;
   }
 }
