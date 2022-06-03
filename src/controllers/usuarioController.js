bf4d3d1471838af8a8d292707d0e1058e463cddd
Jeef-Jr <jefferson.goncalvesfo@sptech.school>
2022-06-02 21:46:41 -0300

    Formulário de válidações concluído.

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,51 @@
+const usuarioModel = require("../models/usuarioModel");
+
+async function verifyLogin(req, res) {
+  const login = await req.body.login;
+
+  usuarioModel.verifyLogin(login).then((response) => {
+    res.json({
+      response,
+    });
+  });
+}
+
+function entrar(req, res) {
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
+
+  if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está indefinida!");
+  } else {
+    usuarioModel
+      .entrar(email, senha)
+      .then((resultado) => {
+        console.log(`\nResultados encontrados: ${resultado.length}`);
+        console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
+
+        if (resultado.length == 1) {
+          console.log(resultado);
+          res.json(resultado[0]);
+        } else if (resultado.length == 0) {
+          res.status(403).send("Email e/ou senha inválido(s)");
+        } else {
+          res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+        }
+      })
+      .catch((erro) => {
+        console.log(erro);
+        console.log(
+          "\nHouve um erro ao realizar o login! Erro: ",
+          erro.sqlMessage
+        );
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
+}
+
+module.exports = {
+  entrar,
+  verifyLogin,
+};
