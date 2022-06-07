1dd8acf54be0bbb676581688e99f81222ce8064c
Jeef-Jr <jefferson.goncalvesfo@sptech.school>
2022-06-06 23:11:02 -0300

    Validação de isLogin

--- public/component/global/index.js
+++ public/component/global/index.js
@@ -34,104 +34,122 @@ function sair() {
 }
 
 function darLike(idLang) {
-  fetch("/like/active", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      idUser: id,
-      idLang,
-    }),
-  }).then((response) => {
-    response.json().then((data) => {
-      const mensagem = data.mensagem;
+  if (id > 0) {
+    fetch("/like/active", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        idUser: id,
+        idLang,
+      }),
+    }).then((response) => {
+      response.json().then((data) => {
+        const mensagem = data.mensagem;
 
-      if (mensagem == "success") {
-        listarLike(idLang);
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener("mouseenter", Swal.stopTimer);
-            toast.addEventListener("mouseleave", Swal.resumeTimer);
-          },
-        });
+        if (mensagem == "success") {
+          listarLike(idLang);
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
 
-        switch (idLang) {
-          case 1: {
-            console.log("entrou");
-            Toast.fire({
-              icon: "success",
-              title: "Você curtiu ReactJs 🎉",
-            });
-            break;
+          switch (idLang) {
+            case 1: {
+              console.log("entrou");
+              Toast.fire({
+                icon: "success",
+                title: "Você curtiu ReactJs 🎉",
+              });
+              break;
+            }
+            case 2: {
+              Toast.fire({
+                icon: "success",
+                title: "Você curtiu NodeJs 🎉",
+              });
+              break;
+            }
+            case 3: {
+              Toast.fire({
+                icon: "success",
+                title: "Você curtiu PHP 7 🎉",
+              });
+              break;
+            }
+            case 4: {
+              Toast.fire({
+                icon: "success",
+                title: "Você curtiu Java 🎉",
+              });
+              break;
+            }
+            case 5: {
+              Toast.fire({
+                icon: "success",
+                title: "Você curtiu Python 🎉",
+              });
+              break;
+            }
+            case 6: {
+              Toast.fire({
+                icon: "success",
+                title: "Você curtiu Flutter 🎉",
+              });
+              break;
+            }
+            case 7: {
+              Toast.fire({
+                icon: "success",
+                title: "Você curtiu Dart 🎉",
+              });
+              break;
+            }
           }
-          case 2: {
-            Toast.fire({
-              icon: "success",
-              title: "Você curtiu NodeJs 🎉",
-            });
-            break;
-          }
-          case 3: {
-            Toast.fire({
-              icon: "success",
-              title: "Você curtiu PHP 7 🎉",
-            });
-            break;
-          }
-          case 4: {
-            Toast.fire({
-              icon: "success",
-              title: "Você curtiu Java 🎉",
-            });
-            break;
-          }
-          case 5: {
-            Toast.fire({
-              icon: "success",
-              title: "Você curtiu Python 🎉",
-            });
-            break;
-          }
-          case 6: {
-            Toast.fire({
-              icon: "success",
-              title: "Você curtiu Flutter 🎉",
-            });
-            break;
-          }
-          case 7: {
-            Toast.fire({
-              icon: "success",
-              title: "Você curtiu Dart 🎉",
-            });
-            break;
-          }
+        } else {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 4000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Você não pode descurti! Sinto muito... 😵‍💫",
+          });
         }
-      } else {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 4000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener("mouseenter", Swal.stopTimer);
-            toast.addEventListener("mouseleave", Swal.resumeTimer);
-          },
-        });
-        Toast.fire({
-          icon: "error",
-          title: "Você não pode descurti! Sinto muito... 😵‍💫",
-        });
-      }
+      });
     });
-  });
+  } else {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 4000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
+    Toast.fire({
+      icon: "error",
+      title: "Você precisa estar logado. 🤪",
+    });
+  }
 }
 
 function listarLike(idLang) {
